9c1767bdeae3fb4ca43727abcdb13cbd74f8b40d
1lyasm <il.mystafa@gmail.com>
2024-01-18 04:28:06 +0300

    Fix pair count bug

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ fn print_hashmap<K: std::fmt::Debug, V: std::fmt::Debug>(
     print!("\n");
 }
 
-fn fill_freq_items<B: std::io::BufRead>(
+fn fill_freq_items(
     item_counts: &mut std::collections::HashMap<String, i64>,
-    lines: std::io::Lines<B>,
+    lines: &Vec<String>,
     support: i64,
-) {
-    for line in lines {
-        let words: Vec<_> = line.as_ref().unwrap().split(" ").collect();
+) -> Result<(), Box<dyn std::error::Error>> {
+    for line in &lines[1..] {
+        let words: Vec<_> = line.split(" ").collect();
         for word in words {
             item_counts
                 .entry(word.to_owned())
@@ -28,14 +28,19 @@ fn fill_freq_items<B: std::io::BufRead>(
 
     item_counts.retain(|_, value| *value >= support);
     print_hashmap(&item_counts);
+
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input_file_name = "input.txt".to_string();
     let input_file = std::fs::File::open(input_file_name)?;
-    let mut lines = std::io::BufReader::new(input_file).lines();
+    let lines: Vec<String> = std::io::BufReader::new(input_file)
+        .lines()
+        .collect::<Result<_, _>>()
+        .unwrap();
 
-    let first_line = lines.next().unwrap()?;
+    let first_line = lines.get(0).unwrap();
     let first_line_words: Vec<_> = first_line.split(" ").collect();
 
     let support = first_line_words.get(0).unwrap().parse::<i64>()?;
@@ -45,9 +50,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("main: confidence: {}", confidence);
 
     let mut item_counts = std::collections::HashMap::<String, i64>::new();
-    fill_freq_items(&mut item_counts, lines, support);
+    fill_freq_items(&mut item_counts, &lines, support)?;
 
+    let mut itemset_counts = std::collections::HashMap::<(String, String), i64>::new();
+    for line in &lines[1..] {
+        let words: Vec<_> = line.split(" ").collect();
+        for i in 0..words.len() {
+            for j in i + 1..words.len() {
+                let mut left_word = words.get(i).unwrap().to_string();
+                let mut right_word = words.get(j).unwrap().to_string();
 
+                if left_word.gt(&right_word) {
+                    let temp = right_word.clone();
+                    right_word = left_word.clone();
+                    left_word = temp.clone();
+                }
+
+                if item_counts.contains_key(&left_word) && item_counts.contains_key(&right_word) {
+                    itemset_counts
+                        .entry((left_word.clone(), right_word.clone()))
+                        .and_modify(|counter| *counter += 1)
+                        .or_insert(1);
+                }
+            }
+        }
+    }
+    print_hashmap(&itemset_counts);
 
     Ok(())
 }
